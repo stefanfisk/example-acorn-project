66fa5b61a27987cd73731ab0d7633ffc3024d384
Stefan Fisk <contact@stefanfisk.com>
2021-03-05 12:13:29 +0100

    Split css/js into front/admin/editor

--- app/Providers/AssetsServiceProvider.php
+++ app/Providers/AssetsServiceProvider.php
@@ -6,6 +6,8 @@ use App\Traits\AddsHooksTrait;
 use Roots\Acorn\Assets\AssetsServiceProvider as BaseServiceProvider;
 
 use function Roots\asset;
+use function wp_enqueue_script;
+use function wp_enqueue_style;
 
 class AssetsServiceProvider extends BaseServiceProvider
 {
@@ -13,12 +15,46 @@ class AssetsServiceProvider extends BaseServiceProvider
 
     public function boot()
     {
-        $this->addAction('wp_enqueue_scripts', 'enqueueAssets');
+        $this->addAction('wp_enqueue_scripts');
+        $this->addAction('admin_enqueue_scripts');
+        $this->addAction('enqueue_block_editor_assets');
     }
 
-    public function enqueueAssets()
+    public function wpEnqueueScripts()
     {
-        wp_enqueue_script('app/app.js', asset('js/app.js')->uri(), [], null, true);
-        wp_enqueue_style('app/app.css', asset('css/app.css')->uri(), false, null);
+        $this->enqueueScript('front');
+        $this->enqueueStyle('front');
+    }
+
+    public function adminEnqueueScripts()
+    {
+        $this->enqueueScript('admin');
+        $this->enqueueStyle('admin');
+    }
+
+    public function enqueueBlockEditorAssets()
+    {
+        add_theme_support('editor-styles');
+
+        $this->enqueueScript('editor');
+        $this->enqueueStyle('editor', ['wp-edit-blocks']);
+    }
+
+    /**
+     * @param string $handle
+     * @param string[] $deps
+     */
+    protected function enqueueScript($handle, $deps = [])
+    {
+        wp_enqueue_script("app/$handle", asset("js/$handle.js")->uri(), $deps, null, true);
+    }
+
+    /**
+     * @param string $handle
+     * @param string[] $deps
+     */
+    protected function enqueueStyle($handle, $deps = [])
+    {
+        wp_enqueue_style("app/$handle", asset("css/$handle.css")->uri(), $deps, false, null);
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -5,15 +5,13 @@ const mix = require('laravel-mix');
 mix.options({ clearConsole: false });
 mix.setPublicPath('./public/content/themes/app/dist');
 
-mix.css('resources/css/app.css', 'css').css('resources/css/editor.css', 'css');
+mix.css('resources/css/front.css', 'css')
+    .css('resources/css/admin.css', 'css')
+    .css('resources/css/editor.css', 'css');
 
-mix.js('resources/js/app.js', 'js')
-    .autoload({ jquery: ['$', 'window.jQuery'] })
-    .extract();
-
-// mix
-//   .copyDirectory('resources/images', 'public/images')
-//   .copyDirectory('resources/fonts', 'public/fonts');
+mix.js('resources/js/front.js', 'js')
+    .js('resources/js/admin.js', 'js')
+    .js('resources/js/editor.js', 'js');
 
 mix.sourceMaps();
 mix.version();
